b7ac49d5bd1f6ff787d2c9b9407a54e9b560c77f
Khoi Huynh <huynh.lu.trong.khoi@styl.solutions>
2021-05-19 14:48:36 +0700

    add todoRouters

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 const corsOptions = {
-    origin: "http://localhost:8081",
+  origin: "http://localhost:8081",
 }
 
 app.use(cors(corsOptions));
@@ -13,11 +13,9 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 
-
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
-
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
